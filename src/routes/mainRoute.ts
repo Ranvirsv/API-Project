ea6e4b5084408b89c73e71637f18b5c2a1e6fda0
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-09-04 19:14:42 -0400

    adding dotenv functionality

--- src/routes/mainRoute.ts
+++ src/routes/mainRoute.ts
@@ -1,9 +1,24 @@
 import express, { Request, Response } from "express";
+import { createClient } from "redis";
+import { configDotenv } from "dotenv";
+
+configDotenv();
 
 const getData = express.Router();
+const uri: string = process.env["DB_URL"]!;
+const password: string = process.env["PASSWORD"]!;
+
+const client = createClient({
+  password: password,
+  socket: {
+    host: uri,
+    port: 15170,
+  },
+});
 
 getData.get("/api", async (_req: Request, res: Response) => {
   res.status(200).json({ message: "you got it!" });
+  client.connect().then((res) => console.log(res));
 });
 
 export { getData };
